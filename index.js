cba205aa21f8a805267eaa079899a5530d61e01e
Johnny Hoang <Johnny@MacBook-Pro.local>
2017-09-01 13:37:34 -0400

    Add FileAdapter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var FileAdapter = require('parse-server-fs-adapter')
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -101,6 +102,10 @@ var api = new ParseServer({
     //optional setting to set a validity duration for password reset links (in seconds) 
     // resetTokenValidityDuration: 24*60*60, // expire after 24 hours 
   }
+
+  fileAdapter: new FileAdapter({
+    filesSubDirectory: './CurbsideConsult'
+  })
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
