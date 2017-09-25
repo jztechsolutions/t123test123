2ad5a20f8eb989b67754bdfa750e9e25c53e9f83
Johnny Hoang <Johnny@MacBook-Pro.local>
2017-09-25 16:56:14 -0400

    Test Cloud Code

--- cloud/main.js
+++ cloud/main.js
@@ -4,21 +4,23 @@ Parse.Cloud.define('Hello', function(request, response) {
 
 Parse.Cloud.define('SendEmail', function(request, response) {
 
-  var mailgun = require('mailgun-js');
-  mailgun.initialize('bodybookapps.com', 'key-77d43d079cb3f40d2c99d8da46a7c452');
-  mailgun.sendEmail({
-    to: "huy.johnny@gmail.com",
-    from: "Mailgun@CloudCode.com",
-    subject: "Hello from Cloud Code!",
-    text: "Using Parse and Mailgun is great!"
-  }, {
-    success: function(httpResponse) {
-        console.log(httpResponse);
-        response.success("Email sent!");
-    },
-    error: function(httpResponse) {
-       console.error(httpResponse);
-       response.error("Uh oh, something went wrong");
-    }
+  var mailgun = require('mailgun-js')({apiKey: 'key-77d43d079cb3f40d2c99d8da46a7c452', domain: 'bodybookapps.com'});
+  
+  var mail = {
+                from: 'Mailgun@CloudCode.com',
+                to: "huy.johnny@gmail.com",
+                subject: "Hello from Cloud Code!",
+                body: 'Using Parse and Mailgun is great!',
+                html: htmlBody
+            };
+
+  mailgun.messages().send(mail, function (sendError, body) {
+    if (error) {
+      inspect(sendError, 'mailgun sendError');
+      response.error("Uh oh, something went wrong");
+    } else {
+      inspect(body, 'Successfully send RECEIPT email to "' + toEmail + '"');
+      response.success("Email sent!");
+    }            
   });
 });
