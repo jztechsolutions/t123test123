ea238d553b7f33cdc749831638e147005c1afa32
Johnny Hoang <Johnny@MacBook-Pro.local>
2017-09-26 17:59:44 -0400

    Test Cloud Code

--- cloud/main.js
+++ cloud/main.js
@@ -78,14 +78,7 @@ Parse.Cloud.define("AddNewProfile", function(request, response){
         currentUserQuery.get(userId)
           .then(function(user){
             user.set("userProfileObjId", newProfile);
-            user.save(null, {
-              success: function() {
-                response.success(user);
-              },
-              error: function(userProfile, error) {                  
-                response.error(error);
-              }
-            });
+            return user.save(null, {useMasterKey: true});
           })
           .catch(function(error){
             //Delete the saved profile if can't connect with username
