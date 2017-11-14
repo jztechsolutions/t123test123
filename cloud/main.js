120cc3d977166043c57b40cf7543d2922fbc95d2
Johnny Hoang <Johnny@MacBook-Pro.local>
2017-11-14 14:25:11 -0500

    Added Email to Profile

--- cloud/main.js
+++ cloud/main.js
@@ -340,6 +340,7 @@ Parse.Cloud.define("AddNewProfile", function(request, response){
           userProfile.set("pri_spec",request.params.pri_spec);  
           userProfile.set("med_sch",request.params.med_sch);
           userProfile.set("grad_yr",request.params.grad_yr);
+          userProfile.set("email",request.params.email);
 
           userProfile.save(null, {
             success: function(newProfile) {
