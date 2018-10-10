a7a5c54e38d4ae0be761f4b4057d7ef16296a0a8
JohnnyH <JHH@JsMacPro.local>
2018-10-10 09:58:57 -0400

    Fix resend issue

--- cloud/main.js
+++ cloud/main.js
@@ -14,7 +14,7 @@ Parse.Cloud.define('Hello', function(request, response) {
 
 function sendInvitationSMS(senderName, recieverName, smsNumbSendTo, token)
 {
-  var invitationMSG  = recieverName + ", please join my Curbside Consult network.\nYou can start by downloading the app today and exploring it. https://goo.gl/tuKMWS\nFrom Dr."+senderName;  
+  var invitationMSG  = recieverName + ", please join my Curbside Consult network.\nYou can start by downloading the app today and exploring it. https://goo.gl/tuKMWS\nFrom "+senderName;  
 
   const accountSid = 'AC4b51bbdcaae206f74fff39eee9549be6';
   const authToken = '5af7ac55302d113a233db59953a0c215';
